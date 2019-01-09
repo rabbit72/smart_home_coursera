cccd939675d1c476bd80a6f2c5c9e21a827f4c42
Daniil Shadrin <rabbit72rus@gmail.com>
2019-01-09 22:17:05 +0300

    add limits and default values

--- coursera_house/core/form.py
+++ coursera_house/core/form.py
@@ -2,7 +2,11 @@ from django import forms
 
 
 class ControllerForm(forms.Form):
-    bedroom_target_temperature = forms.IntegerField()
-    hot_water_target_temperature = forms.IntegerField()
+    bedroom_target_temperature = forms.IntegerField(
+        max_value=50, min_value=16, initial=21
+    )
+    hot_water_target_temperature = forms.IntegerField(
+        max_value=90, min_value=24, initial=80
+    )
     bedroom_light = forms.BooleanField()
     bathroom_light = forms.BooleanField()
