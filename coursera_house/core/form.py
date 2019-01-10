ef248993abd1921b81361d9f5d54370e79a12d85
Daniil Shadrin <rabbit72rus@gmail.com>
2019-01-10 23:37:07 +0300

    set required

--- coursera_house/core/form.py
+++ coursera_house/core/form.py
@@ -8,5 +8,5 @@ class ControllerForm(forms.Form):
     hot_water_target_temperature = forms.IntegerField(
         max_value=90, min_value=24, initial=80
     )
-    bedroom_light = forms.BooleanField()
-    bathroom_light = forms.BooleanField()
+    bedroom_light = forms.BooleanField(required=False)
+    bathroom_light = forms.BooleanField(required=False)
